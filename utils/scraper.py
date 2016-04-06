625acfc1b35142532613019dfd3ddb996dfb2131
Benjamin Field <benjamin.field@gmail.com>
2016-04-06 15:58:03 -0400

    Revamp spynner scraper.

--- utils/scraper.py
+++ utils/scraper.py
@@ -1,34 +1,118 @@
-"""Goof around with spynner.
+"""Grab bbref data with spynner.
 
-runjs directly
-get rid of jupyter - user agent
+Get csv and html data, store in file
+and move to correct location.
 """
 
 import spynner
 import os
 from bs4 import BeautifulSoup
-import pyquery
+# import pyquery
 
-url = 'http://www.baseball-reference.com/leagues/MLB/2016-standard-batting.shtml'
-# useragent = "Mozilla/5.0 (Windows NT 6.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1"
-br = spynner.Browser()
-# br.wait_load(timeout=30)
-br.load(url, load_timeout=15)
+# base url for testing, need to write script
+# to generate url's for other years
+url_start = 'http://www.baseball-reference.com/leagues/MLB/'
+url_end = '-standard-batting.shtml'
+year = '2016'
 
-br.create_webview()
-br.show()
-quals = '''$('input[type="checkbox"]').click()'''
-dl_links = '''$('span[tip$="Excel"]')'''
+# example of changing browser, not in use
+# useragent = "Mozilla/5.0 (Windows NT 6.1; rv:7.0.1)
+# Gecko/20100101 Firefox/7.0.1"
+
+
+def url_maker():
+    """Make url."""
+    pass
+
+url = url_start + year + url_end
+
+
+def get_data(url, name):
+    """Grab csv and html from bbref url and write to data folder."""
+    """Will overwrite existing files.
+    Runtime is about 30sec per page.
+    -> Fielding data would take about 5mins.
+    """
+    # make the paths for the files
+    # and the directory if needed
+    # cwd = os.path.getcwd()
+    stats_dir = os.path.join(os.pardir, 'data', 'data' + year)
+    if os.path.isdir(stats_dir) is False:
+        print "Need to make directory."
+        os.mkdir(stats_dir)
+    csv_path = os.path.join(stats_dir, name + '.csv')
+    html_path = os.path.join(stats_dir, name + '.shtml')
+    # start up spynner
+    br = spynner.Browser()
+    # url = url_start + year + url_end
+    # the page takes a while to load, 10 was too little
+    br.load(url, load_timeout=15)
+    # br.create_webview()
+    # may want to get rid of show() - nice for debugging.
+    br.show()
+    br.load_jquery(True)
+    # unhide non-qualifiers
+    br.click('input[type="checkbox"]')
+    br.wait_load()
+    # grab the html before changing to csv
+
+    with open(html_path, 'w') as f:
+        f.write(br.html.encode('utf-8'))
+
+    print "HTML written."
+    # convert table to csv on page. Will be contained in only <pre>
+    br.runjs("""jQuery('span[tip$="values"]:last').click()""")
+    br.wait(10)
+
+    soup = BeautifulSoup(br.html, 'html.parser')
+    pre_section = soup.find('pre').get_text()
+    with open(csv_path, 'w') as p:
+        p.write(pre_section.encode('utf-8'))
+
+    print "CSV written."
+    # f(x) freezing in REPL after successful execution
+    # tried the two lines below; didn't help
+    # br.destroy_webview()
+    br.close()
+    # print "Please don't freeze."
+    return None
+
+
+def do_stuff():
+    """Test to make sure REPL freeze doesn't screw things."""
+    h = os.getcwd()
+    g = os.path.dirname(h)
+    h = os.path.join(g, 'data', 'data' + year)
+    if os.path.isdir(h):
+        print 'isdir'
+    else:
+        os.mkdir(h)
+        print "made new directory for {}".format(year)
+
+    print g
+    return h, g
+
+
+def main():
+    """Tester."""
+    get_data()
+    cwd, dirstr = do_stuff()
+    return cwd, dirstr
+
+# the jQuery/pyQuery finder for "Hide all non-qualifiers"
+# quals = '''$('input[type="checkbox"]').click()'''
+# the jQ for the download.
+# dl_links = '''$('span[tip$="Excel"]')'''
 # db_links[0] is teams [1] is players
 
-br.load_jquery(True)
-br.click('input[type="checkbox"]')
-br.wait_load()
+
 # br.show()
 
 target = """<span tip="Get a downloadable file suitable for Excel"
-            class="tooltip" onclick="sr_download_data('players_standard_batting');
-            try { ga('send','event','Tool','Action','Export'); } catch (err) {}">
+            class="tooltip"
+            onclick="sr_download_data('players_standard_batting');
+            try { ga('send','event','Tool','Action','Export'); }
+            catch (err) {}">
             Export</span>"""
 
 js = """sr_download_data('players_standard_batting');
@@ -57,15 +141,7 @@ js = """sr_download_data('players_standard_batting');
 # alt technique click to change to csv, grab data
 # prior to converting the data we need the html links
 
-f = open('stats_html', 'w')
-f.write(br.html.encode('utf-8'))
-f.close
-
-
-# converts to csv on page contained in <pre>
-br.runjs("""jQuery('span[tip$="values"]:last').click()""")
-
-br.wait(10)
+'''
 pyq_doc = pyquery.PyQuery(br.html)
 # the comma separated data is in the only 'Pre' tags
 csv = pyq_doc('pre').text()
@@ -78,14 +154,6 @@ f = open('trial', 'w')
 f.write(csv.encode('utf-8'))
 f.close
 
-soup = BeautifulSoup(br.html, 'html.parser')
-pre_section = soup.find('pre').get_text()
-print type(pre_section)
-print len(pre_section)
-with open('bsTest', 'w') as p:
-    p.write(pre_section.encode('utf-8'))
-
-
 # markup = br._get_html()
-
+'''
 print os.getcwd()
