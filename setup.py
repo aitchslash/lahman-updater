8957c459fd45396cccf882fe876cb4221829cfef
Benjamin Field <benjamin.field@gmail.com>
2016-05-10 16:05:58 -0400

    put requirements in setup

--- setup.py
+++ setup.py
@@ -17,5 +17,8 @@ setup(
     author_email='benjamin.field@gmail.com',
     url='https://github.com/aitchslash',
     license=license,
+    install_requires=['spynner==2.19',
+                      'PyMySQL==0.6.7',
+                      'beautifulsoup4==4.4.1'],
     packages=find_packages()
 )
